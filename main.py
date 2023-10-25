85cb5e725f4dec97f435cc3fd0948a923d87968d
OKeylo <zabaev228@gmail.com>
2023-10-25 10:55:33 +0500

    feat(main): add get_cars and add_car endpoints

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
 from fastapi import FastAPI
+import json
+from model import Car
+
 
 app = FastAPI()
 
+
 @app.get("/cars")
 def get_cars():
-    return {"cars": []}
+    with open("cars.json") as file:
+        data = json.load(file)["data"]
+    return {"cars": data}
+
+
+@app.post("/cars")
+def add_car(car: Car):
+    try:
+        with open("cars.json", "r") as file:
+            data = json.load(file)
+            data["data"].append(car)
+        with open("cars.json", "w") as file:
+            json.dump(data, file)
+    except FileNotFoundError or json.decoder.JSONDecodeError:
+        with open("cars.json", "w") as file:
+            json.dump({"data": [car]}, file)
+    return {"status": "success"}
