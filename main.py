62c00be2549712b99e4761165ad0bc40d96ee36e
OKeylo <zabaev228@gmail.com>
2023-10-26 21:57:48 +0500

    feat(main): add remove car endpoint

--- main.py
+++ main.py
@@ -8,9 +8,12 @@ app = FastAPI()
 
 @app.get("/cars")
 def get_cars():
-    with open("cars.json") as file:
-        data = json.load(file)["data"]
-    return {"cars": data}
+    try:
+        with open("cars.json") as file:
+            data = json.load(file)["data"]
+        return {"cars": data}
+    except:
+        return {"cars": []}
 
 
 @app.post("/cars")
@@ -21,7 +24,18 @@ def add_car(car: Car):
             data["data"].append(car)
         with open("cars.json", "w") as file:
             json.dump(data, file)
-    except FileNotFoundError or json.decoder.JSONDecodeError:
+    except json.decoder.JSONDecodeError:
         with open("cars.json", "w") as file:
             json.dump({"data": [car]}, file)
+    return {"status": "success"}
+
+
+@app.delete("/cars/{car_id}")
+def remove_car(car_id: str):
+    with open("cars.json", "r") as file:
+        data: list[Car] = json.load(file)
+        new_data = [car for car in data["data"] if not (car["id"] == car_id)]
+        data["data"] = new_data
+    with open("cars.json", "w") as file:
+        json.dump(data, file)
     return {"status": "success"}
