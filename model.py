d7dbc8f316784a0f8eacc848eb499e9f4d82f3dd
OKeylo <zabaev228@gmail.com>
2023-11-13 18:38:24 +0500

    refactor(model): remove gos_nomer field in Car

--- model.py
+++ model.py
@@ -4,7 +4,16 @@ from typing_extensions import TypedDict
 class Car(TypedDict):
     id: str
     author_id: str
-    gos_nomer: str
+    mark: str
+    model: str
+    year: int
+    price: int
+    fuel: float
+    power: int
+    mileage: int
+
+class addCar(TypedDict):
+    author_id: str
     mark: str
     model: str
     year: int
